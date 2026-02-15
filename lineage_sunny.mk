9210b29eb2841bc5b533b8c9be7aa51d4a2db6ef
ViaanROM <viaan152013@gmail.com>
2026-02-15 15:19:33 +0530

    Bringup for LineageOS

--- lineage_sunny.mk
+++ lineage_sunny.mk
@@ -11,7 +11,7 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sunny device
 $(call inherit-product, device/xiaomi/sunny/device.mk)
 
-# Inherit some common PixelOS stuff.
+# Inherit some common LineageOS stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 
