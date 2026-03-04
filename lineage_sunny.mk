059a4fb2a0cc350a94602459769b0d45ec6bcdad
ViaanROM <viaan152013@gmail.com>
2026-03-04 10:24:11 +0530

    sunny : Adapt for LineageOS

--- lineage_sunny.mk
+++ lineage_sunny.mk
@@ -11,8 +11,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sunny device
 $(call inherit-product, device/xiaomi/sunny/device.mk)
 
-# Inherit some common Project InfinityX stuff.
-$(call inherit-product, vendor/infinity/config/common_full_phone.mk)
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Include Dolby Atmos
 $(call inherit-product, hardware/dolby/dolby.mk)
