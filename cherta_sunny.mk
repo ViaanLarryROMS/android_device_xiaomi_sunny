d152d18aead17f147acc4096344ef47ec931d2e4
ViaanROM <viaan152013@gmail.com>
2026-03-25 18:53:40 +0530

    sunny : Adapt for Project CHerta
    sunny : Adapt for Project CHerta
    
    * Adapt Pixel -> CHerta

--- cherta_sunny.mk
+++ cherta_sunny.mk
@@ -11,8 +11,8 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from sunny device
 $(call inherit-product, device/xiaomi/sunny/device.mk)
 
-# Inherit some common PixelOS stuff.
-$(call inherit-product, vendor/custom/config/common_full_phone.mk)
+# Inherit some common CHerta stuff.
+$(call inherit-product, vendor/cherta/config/common_full_phone.mk)
 TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
