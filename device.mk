f4a4245a22fff63f3cc7b906c30a61538e1591d6
ViaanROM <viaan152013@gmail.com>
2026-04-05 16:13:27 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -15,9 +15,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_ven
 
 # Add common definitions for Qualcomm
 $(call inherit-product, hardware/qcom-caf/common/common.mk)
-
-# Include GCAM
-$(call inherit-product, vendor/gcam/gcam-vendor.mk)
  
 # A/B
 AB_OTA_POSTINSTALL_CONFIG += \
